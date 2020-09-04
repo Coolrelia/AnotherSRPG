1d3b08165de765b77b3def6b24922de1e8fb921d
Coolrelia <sammyrludwig@gmail.com>
2020-09-04 15:47:22 -0400

    Added Movement System

---
+++ AnotherSRPG/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    public Unit selectedUnit;
+
+    public void ResetTiles()
+    {
+        foreach(Tile tile in FindObjectsOfType<Tile>())
+        {
+            tile.Reset();
+        }
+    }
+}
---
+++ AnotherSRPG/Assets/Scripts/Tile.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    private SpriteRenderer rend;
+
+    public float hoverAmount;
+
+    public LayerMask obstacleLayer;
+
+    public Sprite tile;
+    public Sprite highlightedTile;
+    public bool isWalkable;
+    GameMaster gm;
+
+    private void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        gm = FindObjectOfType<GameMaster>();
+    }
+
+    private void OnMouseEnter()
+    {
+        transform.localScale += Vector3.one * hoverAmount;
+    }
+    private void OnMouseExit()
+    {
+        transform.localScale -= Vector3.one * hoverAmount;
+    }
+
+    public bool IsClear()
+    {
+        Collider2D obstacle = Physics2D.OverlapCircle(transform.position, 0.2f, obstacleLayer);
+        if(obstacle != null)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public void Highlight()
+    {
+        rend.sprite = highlightedTile;
+        isWalkable = true;
+    }
+
+    public void Reset()
+    {
+        rend.sprite = tile;
+        isWalkable = false;
+    }
+
+    void OnMouseDown()
+    {
+        if(isWalkable == true && gm.selectedUnit != null)
+        {
+            gm.selectedUnit.Move(this.transform.position);
+        }
+    }
+}
