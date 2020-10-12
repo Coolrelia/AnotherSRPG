64d94ba5a6fa87e466db206310d39f7f816b569b
Sammy <sammyrludwig@gmail.com>
2020-10-12 03:14:27 -0400

    Added a cursor
    Added a cursor controlled by the arrow keys.  It stays within the border of the map which can be adjusted in the inspector depending on the size of the map

---
+++ AnotherSRPG/Assets/Scripts/CursorController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+    public int leftMostTile;
+    public int rightMostTile;
+    public int highestTile;
+    public int lowestTile;
+
+    private Vector3 pos;
+
+    private void Start()
+    {
+        pos = transform.position;
+    }
+
+    private void Update()
+    {
+        pos.z = -8;
+
+        if(pos.x >= leftMostTile + 1)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                pos.x -= 1;
+                transform.position = pos;
+            }
+        }
+
+        if(pos.x <= rightMostTile - 1)
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                pos.x += 1;
+                transform.position = pos;
+            }
+        }
+
+        if(pos.y <= highestTile - 1)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                pos.y += 1;
+                transform.position = pos;
+            }
+        }
+
+        if(pos.y >= lowestTile + 1)
+        {
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                pos.y -= 1;
+                transform.position = pos;
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D unit)
+    {
+        Debug.Log("Detected");
+    }
+}
